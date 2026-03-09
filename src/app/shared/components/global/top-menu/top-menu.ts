06099d6dcf70375108561d66ad579ead6941e41d
Sindoval <sindoval321@gmail.com>
2026-03-09 18:06:06 -0300

    disable button register by route

--- src/app/shared/components/global/top-menu/top-menu.ts
+++ src/app/shared/components/global/top-menu/top-menu.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink } from "@angular/router";
+import { NavigationEnd, Router, RouterLink } from "@angular/router";
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-top-menu',
@@ -10,6 +11,27 @@ import { RouterLink } from "@angular/router";
   templateUrl: './top-menu.html',
   styleUrl: './top-menu.scss',
 })
-export class TopMenu {
-  appLogo = "assets/logo.png"
+export class TopMenu implements OnInit, OnDestroy {
+  appLogo = "assets/logo.png";
+  rotaAtual: string = ''
+  inscricaoRota!: Subscription;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.rotaAtual = this.router.url;
+    this.inscricaoRota = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((evento: NavigationEnd) => {
+        this.rotaAtual = evento.url;
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.inscricaoRota.unsubscribe();
+  }
+
+  isOnRouterRegister(): boolean {
+    return this.rotaAtual === '/register'
+  }
 }
